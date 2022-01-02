5e8f8017307e6a6f8155827d77123693db9c3113
axelkjellin <axel.kjellin.self@gmail.com>
2022-01-02 14:49:03 -0300

    classe responsavel por criar o email

---
+++ back-end/app/Mail/ContactMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Contact;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * contact
+     *
+     * @var Contact
+     */
+    private $contact;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from('site-gastronomia@gmail.com', 'Site Gastronomia')
+            ->view('emails.contact.contact-mail', ['contact' => $this->contact]);
+    }
+}
