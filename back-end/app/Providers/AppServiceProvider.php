6628dc420df5c8462f04b038ac13c04b90ce2dd4
axelkjellin <axel.kjellin.self@gmail.com>
2022-01-02 14:58:40 -0300

    dando o bind na interface do repository

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            'App\Repositories\Contact\Contracts\ContactRepositoryInterface',
+            'App\Repositories\Contact\ContactRepositoryEloquent'
+        );
     }
 
     /**
